ad45f520313cc8919bbee5662c05f0eb43050754
tkkwa01 <aoisyanok@gmail.com>
2024-05-07 21:12:05 +0900

    validation

--- validator/internship_validator.go
+++ validator/internship_validator.go
@@ -20,12 +20,12 @@ func (iv *internshipValidator) InternshipValidate(internship domain.Internships)
 		validation.Field(
 			&internship.Company,
 			validation.Required.Error("company is required"),
-			validation.RuneLength(1, 30).Error("limited max 30 char"),
+			validation.RuneLength(1, 1000).Error("limited max 30 char"),
 		),
 		validation.Field(
 			&internship.Title,
 			validation.Required.Error("title is required"),
-			validation.RuneLength(1, 30).Error("limited max 30 char"),
+			validation.RuneLength(1, 3000).Error("limited max 30 char"),
 		),
 		validation.Field(
 			&internship.Salary,
@@ -34,44 +34,44 @@ func (iv *internshipValidator) InternshipValidate(internship domain.Internships)
 		validation.Field(
 			&internship.Period,
 			validation.Required.Error("period is required"),
-			validation.RuneLength(1, 30).Error("limited max 30 char"),
+			validation.RuneLength(1, 3000).Error("limited max 30 char"),
 		),
 		validation.Field(
 			&internship.Selection,
 			validation.Required.Error("selection is required"),
-			validation.RuneLength(1, 10).Error("limited max 10 char"),
+			validation.RuneLength(1, 1000).Error("limited max 10 char"),
 		),
 		validation.Field(
 			&internship.Deadline,
 			validation.Required.Error("deadline is required"),
-			validation.RuneLength(1, 30).Error("limited max 30 char"),
+			validation.RuneLength(1, 3000).Error("limited max 30 char"),
 		),
 		validation.Field(
 			&internship.Contributor,
 			validation.Required.Error("contributor is required"),
-			validation.RuneLength(1, 30).Error("limited max 30 char"),
+			validation.RuneLength(1, 3000).Error("limited max 30 char"),
 		),
 		validation.Field(
 			&internship.Detail,
 			validation.Required.Error("detail is required"),
-			validation.RuneLength(1, 1000).Error("limited max 1000 char"),
+			validation.RuneLength(1, 10000).Error("limited max 1000 char"),
 		),
 		validation.Field(
 			&internship.Occupation,
 			validation.Required.Error("occupation is required"),
-			validation.RuneLength(1, 1000).Error("limited max 1000 char"),
+			validation.RuneLength(1, 10000).Error("limited max 1000 char"),
 		),
 
 		validation.Field(
 			&internship.Flow,
 			validation.Required.Error("flow is required"),
-			validation.RuneLength(1, 1000).Error("limited max 1000 char"),
+			validation.RuneLength(1, 10000).Error("limited max 1000 char"),
 		),
 
 		validation.Field(
 			&internship.Method,
 			validation.Required.Error("method is required"),
-			validation.RuneLength(1, 100).Error("limited max 100 char"),
+			validation.RuneLength(1, 10000).Error("limited max 100 char"),
 		),
 
 		validation.Field(
@@ -82,7 +82,7 @@ func (iv *internshipValidator) InternshipValidate(internship domain.Internships)
 		validation.Field(
 			&internship.Duration,
 			validation.Required.Error("duration is required"),
-			validation.RuneLength(1, 30).Error("limited max 30 char"),
+			validation.RuneLength(1, 3000).Error("limited max 30 char"),
 		),
 	)
 }
