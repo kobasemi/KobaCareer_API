4bca01abd3cc1cc3f0a09ea722d000be82cc6be5
tkkwa01 <aoisyanok@gmail.com>
2024-05-23 21:15:05 +0900

    エラー解決

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
@@ -10,15 +11,20 @@ import (
 
 func NewDB() *gorm.DB {
 	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	user := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PW")
+	host := os.Getenv("MYSQL_HOST")
+	port := os.Getenv("MYSQL_PORT")
+	dbName := os.Getenv("MYSQL_DB")
 
-	url := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("MYSQL_USER"),
-		os.Getenv("MYSQL_PW"),
-		os.Getenv("MYSQL_HOST"),
-		os.Getenv("MYSQL_PORT"),
-		os.Getenv("MYSQL_DB"),
-	)
+	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, password, host, port, dbName)
+
+	fmt.Println("Database URL:", url) // デバッグ用に追加
 
 	db, err := gorm.Open(mysql.Open(url), &gorm.Config{})
 	if err != nil {
