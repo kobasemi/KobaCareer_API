d327f29d024a5b581b19c648f2fbdc722be5267a
tkkwa01 <aoisyanok@gmail.com>
2024-05-23 20:35:53 +0900

    change env

--- db/db.go
+++ db/db.go
@@ -5,13 +5,19 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 func NewDB() *gorm.DB {
 	var err error
 
 	url := fmt.Sprintf(
-		"root:password@tcp(db:3306)/koba-career?charset=utf8mb4&parseTime=True&loc=Local",
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PW"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DB"),
 	)
 
 	db, err := gorm.Open(mysql.Open(url), &gorm.Config{})
